17c1f643a17b050fd3f344c1eec9a1bb551a7b14
pyguru123 <pyguru123@gmail.com>
2022-07-21 13:07:28 +0530

    updated news app

---
+++ News App/app.py
@@ -0,0 +1,19 @@
+import requests
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+api_key = "0d48912cbe864506a076a3b74df00c79"
+endpoint = f"https://newsapi.org/v2/top-headlines?country=in&apiKey={api_key}"
+
+@app.route("/")
+def index():
+	r = requests.get(endpoint)
+	response = r.json()
+	articles = response["articles"]
+	return render_template("index.html", articles=articles)
+
+
+if __name__ == "__main__":
+	app.run(debug=True)
