bbee5f044c744a82e99867345b4d7511f202c74a
pyguru123 <pyguru123@gmail.com>
2022-07-21 13:15:01 +0530

    added country support

--- News App/app.py
+++ News App/app.py
@@ -14,6 +14,14 @@ def index():
 	articles = response["articles"]
 	return render_template("index.html", articles=articles)
 
+@app.route("/<string:country>")
+def fetchCountryHeadlines(country):
+	endpoint = f"https://newsapi.org/v2/top-headlines?country={country}&apiKey={api_key}"
+
+	r = requests.get(endpoint)
+	response = r.json()
+	articles = response["articles"]
+	return render_template("index.html", articles=articles)
 
 if __name__ == "__main__":
 	app.run(debug=True)
