7811647733eb8c9ba3a96795ef08adc1d430d396
pyguru123 <pyguru123@gmail.com>
2022-09-10 13:10:12 +0530

    updated ytdl

--- Youtube downloader/app.py
+++ Youtube downloader/app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from pytube import YouTube
 
 app = Flask(__name__)
 
@@ -6,6 +7,19 @@ app = Flask(__name__)
 def index():
 	return render_template("index.html")
 	
+@app.route("/download", methods=["POST", "GET"])
+def downloadVideo():
+	if request.method == "POST":
+		url = request.form["videourl"]
+		yt = YouTube(url)
+		title = yt.title
+		thumbnail = yt.thumbnail_url
+		streams = yt.streams
+		print(thumbnail)
+		data = [title, thumbnail, streams]
+		
+		return render_template("downloadPage.html", data=data)
 	
+# https://youtu.be/jiY4PgUW7aA
 if __name__ == "__main__":
 	app.run(debug=True)
