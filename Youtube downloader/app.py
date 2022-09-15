b5d211181cde72394c7b2e272baac5ae3960f845
pyguru123 <pyguru123@gmail.com>
2022-09-15 15:43:39 +0530

    fixed download issue

--- Youtube downloader/app.py
+++ Youtube downloader/app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, send_file
 from pytube import YouTube
+from io import BytesIO
 
 app = Flask(__name__)
 
@@ -7,8 +8,8 @@ app = Flask(__name__)
 def index():
 	return render_template("index.html", data=None)
 	
-@app.route("/download", methods=["POST", "GET"])
-def downloadVideo():
+@app.route("/index", methods=["POST", "GET"])
+def fetchVideo():
 	if request.method == "POST":
 		url = request.form["videourl"]
 		if url:
@@ -19,6 +20,22 @@ def downloadVideo():
 			data = [title, thumbnail, streams, yt]
 			
 			return render_template("index.html", data=data)
+
+@app.route("/download/<itag>", methods=["POST", "GET"])
+def downloadVideo(itag):
+	if itag:
+		yt = YouTube("https://youtu.be/jiY4PgUW7aA")
+		video = yt.streams.get_by_itag(str(itag))
+		buffer = BytesIO()
+		video.stream_to_buffer(buffer)
+		buffer.seek(0)
+
+		return send_file(
+			buffer,
+			as_attachment=True,
+			attachment_filename="cool-video.mp4",
+			mimetype="video/mp4",
+		)
 	
 # https://youtu.be/jiY4PgUW7aA
 if __name__ == "__main__":
