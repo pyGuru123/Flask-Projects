f1d2f9bd82280117abbf2c18654086e1707b778a
Prajjwal Pathak <71623767+pyGuru123@users.noreply.github.com>
2022-11-07 12:04:53 +0530

    Updated app.py

--- text2Image OpenAI DALLe2/app.py
+++ text2Image OpenAI DALLe2/app.py
@@ -1,1 +1,29 @@
+from flask import Flask, render_template, request
+import requests
 
+app = Flask(__name__)
+
+key = "Bearer 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"
+endpoint = "https://api.openai.com/v1/images/generations"
+headers = {"Content-Type": "application/json", "Authorization": key}
+
+@app.route("/", methods=["POST", "GET"])
+def home():
+	if request.method == "POST":
+		text = request.form["desc"]
+		data = {
+    "prompt": text,
+    "n": 3,
+    "size": "1024x1024"
+    }
+		r = requests.post(endpoint, json=data,headers=headers)
+		print(r.json())
+		
+		imgs = r.json()["data"]
+		images = [img['url'] for img in imgs]
+		#print(images)
+		return render_template("index.html", images=images, text=text)
+	return render_template("index.html", images=None, text=None)
+	
+if __name__ == "__main__":
+	app.run(debug=True)
