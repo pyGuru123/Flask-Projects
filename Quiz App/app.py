a4bdf148cb31a2a09c5b24dcfc94167563e56e50
pyguru123 <pyguru123@gmail.com>
2022-12-07 11:40:19 +0530

    fixed something

--- Quiz App/app.py
+++ Quiz App/app.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template, url_for, request, session
 from flask_session import Session
 from model import fetchQuestions
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static')
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
@@ -42,9 +42,8 @@ def categoriesPage():
 	if request.method == "POST":
 		username = request.form["username"]
 		password = request.form["password"]
-
-		return render_template('categories.html', username=username, 
-													categories=categories)
+		[session.pop(key) for key in list(session.keys())]
+		return render_template('categories.html', username=username)
 
 @app.route('/instructions', methods=["GET", "POST"])
 def instructionsPage():
@@ -56,18 +55,35 @@ def instructionsPage():
 			session['category'] = category
 		return render_template('instructions.html', data=data)
 
-@app.route('/quiz/<int:id>', methods=["GET", "POST"])
-def quizPage(id):
+@app.route('/quiz', methods=["GET", "POST"])
+def quizPage():
 	if request.method == "POST":
 		if not session.get('questions'):
 			questions = fetchQuestions(session['category'])
 			session['questions'] = questions
-			session['id'] = id
 
-		questions = session['questions']
+	questions = session['questions']
+	id = int(request.form['input'])
+	if id <= 10:
 		question = questions[id-1]
-		session['id'] += 1
-		return render_template('quiz.html', data=questions[id])
+		icon = categories[question[2]]
+
+		data = {
+			'id' : id,
+			'question' : question[1],
+			'category' : question[2],
+			'icon' : icon,
+			'price' : question[3],
+			'pic' : icon.rstrip('.png') + str(id)
+		}
+		print(data)
+
+		return render_template('quiz.html', data=data)
+
+# @app.after_request
+# def after_request(response):
+#     response.headers["Cache-Control"] = "no-cache"
+#     return response
 
 if __name__ == "__main__":
 	app.run(debug=True)
