47ef308ee4bfaf8f906ed9b750e609e2081ab608
pyguru123 <pyguru123@gmail.com>
2023-02-07 00:29:20 +0530

    added mongodb database connection

--- Course Provider Sample Website/app/routes.py
+++ Course Provider Sample Website/app/routes.py
@@ -1,4 +1,5 @@
-from app import app
+from app import app, db
+from app.models import User, Course, Enrollment
 from flask import render_template, request, json, Response
 
 courseData = [{"courseID":"1111","title":"PHP 101","description":"Intro to PHP","credits":3,"term":"Fall, Spring"}, {"courseID":"2222","title":"Java 1","description":"Intro to Java Programming","credits":4,"term":"Spring"}, {"courseID":"3333","title":"Adv PHP 201","description":"Advanced PHP Programming","credits":3,"term":"Fall"}, {"courseID":"4444","title":"Angular 1","description":"Intro to Angular","credits":3,"term":"Fall, Spring"}, {"courseID":"5555","title":"Java 2","description":"Advanced Java Programming","credits":4,"term":"Fall"}]
@@ -12,6 +13,7 @@ def index():
 @app.route('/courses')
 @app.route('/courses/<term>')
 def courses(term="2019"):
+    courseData = Course.objects.all()
     return render_template("courses.html", courseData=courseData, courses=True, term=term)
 
 @app.route('/register')
@@ -44,3 +46,11 @@ def api(id=None):
         jData = courseData[int(id)]
 
     return Response(json.dumps(jData), mimetype="application/json")
+
+@app.route('/user')
+def user():
+    # User(user_id=1, first_name='Bruce', last_name='Wayne', email='bw123@gmail.com', password='password').save()
+    # User(user_id=2, first_name='Mary', last_name='Jane', email='mj@gmail.com', password='password123').save()
+
+    users = User.objects.all()
+    return render_template("user.html", users=users)
--- Course Provider Sample Website/config.py
+++ Course Provider Sample Website/config.py
@@ -2,4 +2,6 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY")
+
+    MONGODB_SETTINGS = {'db' : 'Course_Enrollment'}
     
